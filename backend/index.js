6bc4ea797f3e26ff80311dfb7a23c6993ae2435c
Pravallika <pravallika7780@gmail.com>
2025-06-23 22:30:09 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoDB = require("./db");
-const cors = require('cors');
+const cors = require("cors");
 
 mongoDB();
 
-// Enable CORS for all routes
-app.use(cors());
+const cors = require("cors");
+
+const allowedOrigins = [
+  "http://localhost:3000", // for local development
+  "https://tastyheaven4.netlify.app" // deployed frontend
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello Pravallika   ------");
+  res.send("Hello Pravallika ------");
 });
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
 
-  );
-  next();
-})
-
-
-app.use(express.json())
-app.use('/api',require("./Routes/CreateUser"))
-app.use('/api',require("./Routes/DisplayData"))
-app.use('/api',require("./Routes/OrderData"))
-app.use('/api',require("./Routes/AdminLogin"))
-app.use('/api/admin',require("./Routes/AdminData"))
-
-
-
+// Routes
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
+app.use("/api", require("./Routes/AdminLogin"));
+app.use("/api/admin", require("./Routes/AdminData"));
 
 app.listen(port, () => {
   console.log(`server is running ${port}`);
